94071f35b252736d05606c956e9f56e9e17d4479
Grant Bradley <grantbradleytiger13@gmail.com>
2023-05-18 14:23:25 -0400

    Added individual product selection options

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -20,5 +20,10 @@ namespace Testing.Controllers
 
             return View(products);
         }
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }       
     }
 }
