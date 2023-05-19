9440a32d12b082d5ed8b6e96a5ffa0829a153c6b
Grant Bradley <grantbradleytiger13@gmail.com>
2023-05-19 08:05:06 -0400

    Initial Commit

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -10,5 +10,7 @@ namespace Testing
     {
         public IEnumerable <Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
     }
+
 }
